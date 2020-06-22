78597cc41ebed6dc2564906f4aea739e93f4036f
A00x40 <44728333+A00x40@users.noreply.github.com>
2020-06-22 10:57:19 +0200

    Update LZW.h

--- LZW.h
+++ LZW.h
@@ -9,16 +9,21 @@ using namespace std;
 
 class LZW
 {
-        string In_File_Location;
-        string Encoded_File_Location;
-        string Decoded_File_Location;
-        map<int,string> Dictionary;
-        int dict_Count;
+	string In_File_Location;
+	string Encoded_File_Location;
+	string Decoded_File_Location;
+	map<int,string> Dictionary;
+	int dict_Count;
 public:
-        LZW(pair<char,int>ch[] );
-        void setInFileLocation(string);
-        void setEncodedFileLocation(string = "./encoded");
-        void setDecodedFileLocation(string = "./decoded");
-        void encode();
-        void decode();
+	LZW(pair<char,int>ch[] );
+	void setInFileLocation(string);
+	void setEncodedFileLocation(string = "./encoded");
+	void setDecodedFileLocation(string = "./decoded");
+        
+	void encode();
+	void bitConcat(string&, string&);
+	int binary_to_decimal(string&);
+        
+	void decode();
 };
+
