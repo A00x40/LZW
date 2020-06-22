92289c63088df331045570d9d483dd1815dee0a4
A00x40 <44728333+A00x40@users.noreply.github.com>
2020-06-22 10:07:35 +0200

    Create LZW.h

---
+++ LZW.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <map>
+#include <fstream>
+
+using namespace std;
+
+
+class LZW
+{
+        string In_File_Location;
+        string Encoded_File_Location;
+        string Decoded_File_Location;
+        map<int,string> Dictionary;
+        int dict_Count;
+public:
+        LZW(pair<char,int>ch[] );
+        void setInFileLocation(string);
+        void setEncodedFileLocation(string = "./encoded");
+        void setDecodedFileLocation(string = "./decoded");
+        void encode();
+        void decode();
+};
